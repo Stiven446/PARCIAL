8110d755823b9c9c476e8577c8b0220b0ce1d7a1
Johan Stiven Gallego Restrepo <gallegojohan22@gmail.com>
2025-05-28 03:24:56 -0500

    estadobicicletamodel.java

---
+++ estadobicicletamodel.java
@@ -0,0 +1,12 @@
+package com.biciapi.bici_api.model;
+
+public class EstadoBicicleta {
+    private int idEstado;
+    private String nombreEstado;
+
+    public int getIdEstado() { return idEstado; }
+    public void setIdEstado(int idEstado) { this.idEstado = idEstado; }
+
+    public String getNombreEstado() { return nombreEstado; }
+    public void setNombreEstado(String nombreEstado) { this.nombreEstado = nombreEstado; }
+}
