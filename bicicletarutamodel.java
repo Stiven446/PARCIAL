13318032c31d8bedf3a3093cea13596bd8ea0c9e
Johan Stiven Gallego Restrepo <gallegojohan22@gmail.com>
2025-05-28 03:23:46 -0500

    bicicletarutamodel.java

---
+++ bicicletarutamodel.java
@@ -0,0 +1,8 @@
+package com.biciapi.bici_api.model;
+
+public class BicicletaRuta extends Bicicleta {
+    private float peso;
+
+    public float getPeso() { return peso; }
+    public void setPeso(float peso) { this.peso = peso; }
+}
