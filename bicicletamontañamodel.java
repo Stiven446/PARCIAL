fa3c5f8f6f9c63d16aa65c15a6cb78f3b95872a9
Johan Stiven Gallego Restrepo <gallegojohan22@gmail.com>
2025-05-28 03:23:00 -0500

    bicicletamontañamodel.java

---
+++ bicicletamontañamodel.java
@@ -0,0 +1,8 @@
+package com.biciapi.bici_api.model;
+
+public class BicicletaMontana extends Bicicleta {
+    private String tipoSuspension;
+
+    public String getTipoSuspension() { return tipoSuspension; }
+    public void setTipoSuspension(String tipoSuspension) { this.tipoSuspension = tipoSuspension; }
+}
